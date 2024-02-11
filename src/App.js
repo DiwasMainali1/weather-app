d0b4ac651698778fc9625602f9a6912fab077baf
Diwas Mainali <diwas.sok@gmail.com>
2024-02-11 22:45:44 +1100

    Weather app initialisation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
+
+    const apiKey = 'c9b819d5b04b0d2997e72985defa1198' 
+    const [weatherData, setWeatherData] = useState([{}])
+    const [city, setCity] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <input className="input" placeholder="Enter City..."></input>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
