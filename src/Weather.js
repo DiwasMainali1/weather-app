a59f7872948f8e8f12383b39dc619176b015c225
Diwas Mainali <diwasmainali@Diwass-MacBook-Air.local>
2024-02-13 14:32:33 +1100

    Updated and finished up the CSS elements for the weather data

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Weather.css';
 import { FaSearch } from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
+import {WiHumidity} from 'react-icons/wi';
 
 
 
@@ -11,7 +13,7 @@ export const Weather = () => {
     const [error, setError] = useState('');
 
     const apiKey = "c9b819d5b04b0d2997e72985defa1198";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     
     function eventHandler(event) {
         setCity(event.target.value);
@@ -26,6 +28,7 @@ export const Weather = () => {
                 setError('');
                 console.log(output);
             } else {
+                setWeather(null);
                 setError('Enter a valid city name!');
             }
         }
@@ -51,9 +54,29 @@ export const Weather = () => {
             <div className='weatherContent'>
                 <div className='weatherImage'>
                     <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt=''></img>
+                    <h3 className='weatherdesc'>{weather.weather[0].description}</h3>
+
                 </div>
-            </div>
+                <div className='weatherTemp'>
+                    <h2>{weather.main.temp}°C</h2>
+                </div>
+                <div className='weatherCity'>
+                    <div className='location'>
+                        <MdLocationOn></MdLocationOn>
+                    </div>
+                    <p>{weather.name},<span>{weather.sys.country}</span></p>
+                    <div className='humidity'>
+                        <div className='humidityIcon'>
+                            <WiHumidity></WiHumidity>
+                        </div>
+                        <h3 className='humidityPercent'>{weather.main.humidity}<span>%</span></h3>
+                        <h3 className='humidityHeading'>Humidity</h3>
+                    </div>
+                </div>
+
+                </div>
         }
+
     </div>
 
   )
